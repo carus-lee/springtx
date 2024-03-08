21c0e8f837feb3fb72badccca55317eda9c3244f
carus-lee <leec8.2019@gmail.com>
2024-03-08 11:19:45 +0900

    트랜잭션 AOP 주의사항 - 프록시 내부호출 문제점 확인 - internal(), external()

---
+++ src/test/java/hello/springtx/apply/InternalCallTestV1.java
@@ -0,0 +1,62 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+@Slf4j
+public class InternalCallTestV1 {
+
+    @Autowired CallService callService;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", callService.getClass());
+    }
+
+    @Test
+    void externalCall() {
+        callService.external();
+    }
+
+    @Test
+    void InternalCall() {
+        callService.internal();
+    }
+
+    @TestConfiguration
+    static class InternalCallV1TestConfig {
+
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    static class CallService {
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal();
+        }
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+//            boolean readonly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//            log.info("tx readonly={}", readonly);
+        }
+    }
+}
